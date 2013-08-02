36a8a1da033e971a76a9160e751003e213d1a309
Csaba Henk <csaba@lowlife.hu>
2013-08-02 15:06:34 +0200

    migrate gauguin to ERB

--- gauguin.rb
+++ gauguin.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require 'erb'
 require 'parsefuse'
 
 class FuseMsg
@@ -7,64 +8,8 @@ class FuseMsg
   module Go
     extend self
 
-    def makehead out
-      out <<
-<<GOBLOCK
-package protogen
-
-import (
-	"encoding/binary"
-	"bytes"
-	"log"
-	"unsafe"
-)
-
-func clen(n []byte) int {
-	for i := 0; i < len(n); i++ {
-		if n[i] == 0 {
-			return i
-		}
-	}
-	log.Fatal("terminating zero not found in C string")
-	return -1
-}
-
-GOBLOCK
-     FuseVersion.size == 2 and out <<
-<<GOBLOCK
-const(
-	FuseMajor = #{FuseVersion[0]}
-	FuseMinor = #{FuseVersion[1]}
-)
-
-GOBLOCK
-    end
-
     def opcodemap cn
       cn.sub(/^FUSE_/, "")
-    end
-
-    def makeopcodes out
-      out << "const(\n"
-      Ctypes[:Enum].each { |e,a|
-        a.each { |n,v|
-          v or next
-          out << "\t#{opcodemap n} uint32 = #{v}\n"
-        }
-      }
-      out << ")\n\n"
-
-      out << "var FuseOpnames = [...]string{\n"
-      used = Set.new [nil]
-      Ctypes[:Enum].each { |e,a|
-        a.each { |n,v|
-          used.include? v and next
-          n = opcodemap n
-          out << %Q(\t#{n}: "#{n}",\n)
-          used << v
-        }
-      }
-      out << "}\n\n"
     end
 
     def camelize nam
@@ -88,143 +33,9 @@ GOBLOCK
       end
     end
 
-    def makestruct name, desc, out
-      s = typemap name
-      out <<
-<<GOBLOCK
-// #{s}
-type #{s} struct {
-GOBLOCK
-      desc.each { |f,v|
-        out << "\t#{camelize v} #{typemap f}\n"
-      }
-      out <<
-<<GOBLOCK
-}
-
-func as#{s}(buf []byte) *#{s} {
-	return (*#{s})(unsafe.Pointer(&buf[0]))
-}
-
-func as#{s}Bytesex(buf []byte, o binary.ByteOrder) *#{s} {
-	a#{s} := new(#{s})
-	binary.Read(bytes.NewBuffer(buf), o, a#{s})
-	return a#{s}
-}
-
-GOBLOCK
+    def makeerb erb, out
+      out << ERB.new(erb).result(binding)
     end
-
-    def makestructs out
-      Ctypes[:Struct].each { |s,d|
-        makestruct s, d, out
-      }
-      out <<
-<<GOBLOCK
-type DataCaster struct {
-	AsUint32 func([]byte) uint32
-GOBLOCK
-      Ctypes[:Struct].each_key { |s|
-        t = typemap s
-        out << "\tAs#{t} func([]byte) *#{t}\n"
-      }
-      out <<
-<<GOBLOCK
-}
-
-var NativeDataCaster = DataCaster {
-	AsUint32: func(buf []byte) uint32 { return *(*uint32)(unsafe.Pointer(&buf[0])) },
-GOBLOCK
-      Ctypes[:Struct].each_key { |s|
-        t = typemap s
-	out << "\tAs#{t}: as#{t},\n"
-      }
-      out << "}\n\n"
-      %w[Little Big].each { |e|
-        out <<
-<<GOBLOCK
-var #{e[0]}eDataCaster = DataCaster {
-	AsUint32: func(buf []byte) uint32 { return binary.#{e}Endian.Uint32(buf) },
-GOBLOCK
-        Ctypes[:Struct].each_key { |s|
-          t = typemap s
-          out << "\tAs#{t}: func(buf []byte) *#{t} { return as#{t}Bytesex(buf, binary.#{e}Endian) },\n"
-        }
-        out << "}\n\n"
-      }
-      out << "\n\n"
-    end
-
-    def makeparser fnam, mmap, out
-      out <<
-<<GOBLOCK
-func #{fnam}(datacaster DataCaster, opcode uint32, data []byte) (a []interface{}) {
-	pos := 0
-	a = make([]interface{}, 0, 2)
-	switch opcode {
-GOBLOCK
-      mmap.each do |c,d|
-        d or next
-        d = d.map { |t| typemap t }
-        d[0...-1].include? "[0]byte" and raise "[0]byte type must be trailing"
-        out <<
-<<GOBLOCK
-        case #{opcodemap c}:
-GOBLOCK
-        strings = 0
-        d.each do |t|
-          out << case t
-          when "[0]byte"
-<<GOBLOCK
-		a = append(a, data[pos:])
-GOBLOCK
-          when "string"
-<<GOBLOCK
-		l #{(strings += 1) == 1 ? ":" : ""}= clen(data[pos:])
-		a = append(a, string(data[pos:][:l]))
-		pos += l + 1
-GOBLOCK
-          else
-<<GOBLOCK
-		if len(data[pos:]) >= int(unsafe.Sizeof(#{t}{})) {
-			a = append(a, *datacaster.As#{t}(data[pos:]))
-			pos += int(unsafe.Sizeof(#{t}{}))
-		} else {
-				a = append(a, data[pos:])
-		}
-GOBLOCK
-          end
-        end
-      end
-      out <<
-<<GOBLOCK
-	default:
-		if FuseOpnames[opcode] == "" {
-			log.Printf("warning: unknown opcode %d", opcode)
-		} else {
-			log.Printf("warning: format spec missing for %s", FuseOpnames[opcode])
-		}
-		a = append(a, data)
-	}
-	return
-}
-
-GOBLOCK
-    end
-
-    def makeparsers out
-      Msgmap.each { |c, m|
-         makeparser "Parse#{c}", m, out
-      }
-    end
-
-    def makeall out
-      makehead out
-      makeopcodes out
-      makestructs out
-      makeparsers out
-    end
-
   end
 end
 
@@ -238,5 +49,5 @@ if __FILE__ == $0
     op.on('-m', '--msgdef F') { |v| msgy = v }
   end.parse!
   FuseMsg.import_proto protoh, msgy
-  FuseMsg::Go.makeall $>
+  FuseMsg::Go.makeerb STDIN.read, $>
 end
