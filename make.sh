50821e14b05fe77a978a8a3a90824956102de66b
Csaba Henk <csaba@lowlife.hu>
2016-09-14 08:18:06 +0200

    stop lying about the messages description being JSON -- it's YAML!

--- make.sh
+++ make.sh
@@ -6,6 +6,6 @@ protogendir="$GOPATH/src/parsefuse/protogen"
 
 mkdir -p "$protogendir"
 
-ruby -I. gauguin.rb -m messages.json $* < protogen.erb | gofmt > "$protogendir/protogen.go"
+ruby -I. gauguin.rb -m messages.yaml $* < protogen.erb | gofmt > "$protogendir/protogen.go"
 
 go install parsefuse
